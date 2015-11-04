4ee14ee9f09660116a349d272ddcab75b08244d6
Amy Bakameyer <bymak87@gmail.com>
2015-11-04 00:12:37 -0600

    makes views unique to user

--- todos.js
+++ todos.js
@@ -1,5 +1,6 @@
 Todos = new Mongo.Collection('todos');
 if (Meteor.isClient) {
+  Meteor.subscribe('todos');
   //Template Helpers
   Template.main.helpers({
     todos: function() {
@@ -22,7 +23,7 @@ if (Meteor.isClient) {
     },
     "click .delete-todo": function(){
       if(confirm('Are you sure?')){
-        Meteor.call('deleteTodos');
+        Meteor.call('deleteTodo', this._id);
       }
     }
   });
@@ -31,6 +32,17 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+if (Meteor.isServer) {
+  Meteor.publish('todos', function () {
+    if(!this.userId){
+      return Todos.find();
+    } else {
+      return Todos.find({userId: this.userId});
+    }
+
+  });
+//Meteor Methods
 
 Meteor.methods({
   addTodo: function (text) {
@@ -45,12 +57,19 @@ Meteor.methods({
     });
   },
   deleteTodo: function (todoId){
+    var todo = Todos.findOne(todoId);
+    if( todo.userId !== Meteor.userId()){
+      throw new Meteor.Error('not-authorized');
+    }
     Todos.remove(todoId);
   },
-  setChecked: function (){
-    Todos.update(todoId, {$set:{checked: setChecked()}})
+  setChecked: function (todoId, setChecked){
+    var todo = Todos.findOne(todoId);
+    if( todo.userId !== Meteor.userId()){
+      throw new Meteor.Error('not-authorized');
+    }
+    Todos.update(todoId, {$set:{checked: setChecked}})
   }
-})
-if (Meteor.isServer) {
+});
 
 }
